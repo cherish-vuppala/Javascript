1c9ee5b512f416155fbc8796c9c6a1984b5bc3b2
cherish <cherishvuppala@gmail.com>
2024-11-26 17:59:30 +0530

    add Scope.js to codebase

---
+++ Scope.js
@@ -0,0 +1,13 @@
+// Scope in JavaScript
+
+if (true) {
+  let a = 1; // block scope or local scope
+  const b = 2; // block scope or local scope
+  var c = 3; // global scope
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
