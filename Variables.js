63c9e22200b35a0bf1e1140cff1cd151ef5451d5
cherish <cherishvuppala@gmail.com>
2024-11-26 14:13:55 +0530

    update Variable.js

--- Variables.js
+++ Variables.js
@@ -10,3 +10,10 @@ console.log(typeof isEligible);
 let obj = null;
 console.log(typeof obj);
 
+// Explanation:
+// obj2 is implicitly declared as a global variable (not recommended) and assigned the value 23.
+// typeof obj2 checks its type, which is "number".
+
+obj2 = 23;
+console.log(obj2);
+console.log(typeof obj2);
