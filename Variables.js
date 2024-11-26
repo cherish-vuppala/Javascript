4311c170fccce0e12801b8f88d852c7a2c882863
cherish <cherishvuppala@gmail.com>
2024-11-26 14:10:42 +0530

    add variables to codebase

---
+++ Variables.js
@@ -0,0 +1,12 @@
+let age = 1;
+console.log(typeof age);
+
+num = 1.1;
+console.log(typeof num);
+
+let isEligible = true;
+console.log(typeof isEligible);
+
+let obj = null;
+console.log(typeof obj);
+
